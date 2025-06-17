cd7fee7e4e52402e687facb5058e100868d98630
carloshipol <carlaogaruda30@gmail.com>
2025-06-16 21:47:21 -0300

    feat: configured services api

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private apiUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(offset: number = 0, limit: number = 20): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon?offset=${offset}&limit=${limit}`);
+  }
+
+  getPokemonDetails(name: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon/${name}`);
+  }
 }
