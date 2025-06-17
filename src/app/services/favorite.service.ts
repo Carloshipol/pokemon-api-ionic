ddc5918c0ec2148757b64586f6f9254a9c84764c
carloshipol <carlaogaruda30@gmail.com>
2025-06-16 22:03:16 -0300

    feat: favorite service configured

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -5,5 +5,30 @@ import { Injectable } from '@angular/core';
 })
 export class FavoriteService {
 
+  private storageKey = 'favoritePokemons';
+
   constructor() { }
+
+  getFavorites(): string[] {
+    const favorites = localStorage.getItem(this.storageKey);
+    return favorites ? JSON.parse(favorites) : [];
+  }
+
+  isFavorite(name: string): boolean {
+    const favorites = this.getFavorites();
+    return favorites.includes(name);
+  }
+
+  addFavorite(name: string) {
+    const favorites = this.getFavorites();
+    if (!favorites.includes(name)) {
+      favorites.push(name);
+      localStorage.setItem(this.storageKey, JSON.stringify(favorites));
+    }
+  }
+
+  removeFavorite(name: string) {
+    const favorites = this.getFavorites().filter(item => item !== name);
+    localStorage.setItem(this.storageKey, JSON.stringify(favorites));
+  }
 }
