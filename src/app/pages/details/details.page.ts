72dac4d999f13e0db9d437e5216bebb456e0cbec
carloshipol <carlaogaruda30@gmail.com>
2025-06-16 21:59:42 -0300

    feat: page details configured

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  pokemon: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) { }
 
   ngOnInit() {
+    this.name = this.route.snapshot.paramMap.get('name') || '';
+    this.loadPokemon();
   }
 
+  loadPokemon() {
+    this.pokemonService.getPokemonDetails(this.name).subscribe({
+      next: (data) => {
+        this.pokemon = data;
+      },
+      error: (err) => {
+        console.error('Error fetching Pokémon details:', err);
+      }
+    });
+  }
+
+  getTypes(): string {
+    return this.pokemon.types.map((t: any) => t.type.name).join(', ');
+  }
+
+  getAbilities(): string {
+    return this.pokemon.abilities.map((a: any) => a.ability.name).join(', ');
+  }
 }
